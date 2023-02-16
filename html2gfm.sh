9a69bd459736f8d7a278d1bdf183bfca297b10cb
liruqi <liruqi@gmail.com>
2023-02-16 08:53:16 +0700

    修复锚点链接🔗

--- html2gfm.sh
+++ html2gfm.sh
@@ -1,1 +1,3 @@
-pandoc -f html -t gfm html/安娜·索罗金.html > wiki/安娜·索罗金.md
+INPUT="html/安娜·索罗金.html"
+sed -i "s|https://en.wikipedia.org/wiki/Anna_Sorokin#|#|g" $INPUT
+pandoc -f html -t gfm $INPUT > wiki/安娜·索罗金.md
