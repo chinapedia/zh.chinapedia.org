76c4e7aeea673bba22210ac7262e866f8c27c5bc
liruqi <liruqi@gmail.com>
2023-02-17 08:30:10 +0700

    链接替换到镜像

--- html2gfm.sh
+++ html2gfm.sh
@@ -1,3 +1,5 @@
 INPUT="html/安娜·索罗金.html"
-sed -i "s|https://en.wikipedia.org/wiki/Anna_Sorokin#|#|g" $INPUT
+#https://stackoverflow.com/questions/16745988/sed-command-with-i-option-in-place-editing-works-fine-on-ubuntu-but-not-mac
+sed -i '' "s|https://en.wikipedia.org/wiki/Anna_Sorokin#|#|g" $INPUT
+sed -i '' "s|https://en.wikipedia.org/wiki/|https://en.wiki.chinapedia.org/wiki/|g" $INPUT
 pandoc -f html -t gfm $INPUT > wiki/安娜·索罗金.md
