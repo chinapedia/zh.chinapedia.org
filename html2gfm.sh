b40a0e1bf8da22bf55f4d6375bb6780a8ecb7be0
liruqi <liruqi@gmail.com>
2023-02-19 16:01:03 +0700

    删除重复font标签

--- html2gfm.sh
+++ html2gfm.sh
@@ -3,4 +3,5 @@ INPUT="html/安娜·索罗金.html"
 sed -i '' "s|https://en.wikipedia.org/wiki/Anna_Sorokin#|#|g" $INPUT
 sed -i '' "s|https://en.wikipedia.org/wiki/|https://en.wiki.chinapedia.org/wiki/|g" $INPUT
 sed -i '' "s|<span class=\"mw-editsection\"><span class=\"mw-editsection-bracket\">.*</span></span>|\n|g" $INPUT
+sed -Ei '' "s|<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">([^<]*)</font></font>|\1|g" $INPUT
 pandoc -f html -t gfm $INPUT > wiki/安娜·索罗金.md
