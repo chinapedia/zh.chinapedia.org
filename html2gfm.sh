a393b85ba4e0eee7c196bbf253050006299bf845
liruqi <liruqi@gmail.com>
2023-02-19 15:49:12 +0700

    脚本删除部分编辑文字链接

--- html2gfm.sh
+++ html2gfm.sh
@@ -2,4 +2,5 @@ INPUT="html/安娜·索罗金.html"
 #https://stackoverflow.com/questions/16745988/sed-command-with-i-option-in-place-editing-works-fine-on-ubuntu-but-not-mac
 sed -i '' "s|https://en.wikipedia.org/wiki/Anna_Sorokin#|#|g" $INPUT
 sed -i '' "s|https://en.wikipedia.org/wiki/|https://en.wiki.chinapedia.org/wiki/|g" $INPUT
+sed -i '' "s|<span class=\"mw-editsection\"><span class=\"mw-editsection-bracket\">.*</span></span>|\n|g" $INPUT
 pandoc -f html -t gfm $INPUT > wiki/安娜·索罗金.md
